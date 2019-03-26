3dd609783659f7c5b51876e1a9c7a1275122041d
Christoph Gruener <christophgruener@me.com>
2019-03-26 15:47:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,12 @@
 class CreateShows < ActiveRecord::Migrate
   def change
     create_table :shows do |t|
-      t.
+      t.string :name 
+      t.string :network 
+      t.string :day 
+      t.integer :integer
+    end
+  end
 
 
 end
